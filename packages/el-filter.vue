aa746f21b181e0a23fee710c38949b69264d83dc
山人自有妙计 <wangdaoo@yeah.net>
2021-11-29 15:39:38 +0800

    fix: 格式优化:rocket:

--- packages/el-filter.vue
+++ packages/el-filter.vue
@@ -2,104 +2,104 @@
   <div class="content">
     <template v-for="(item, index) in fieldList">
       <div
-          v-show="!foldBtnHidden && icon === 'close' ? index < count : true"
-          :key="index"
+        v-show="!foldBtnHidden && icon === 'close' ? index < count : true"
+        :key="index"
       >
         <div class="item-style">
           <span class="label-style" :style="{width: width.labelWidth + 'px'}">{{ item.label }}</span>
           <!-- 普通输入框 -->
           <el-input
-              v-if="item.type === 'input'"
-              v-model.trim="defaultData[item.value]"
-              :size="size"
-              :style="{width: width.itemWidth + 'px'}"
-              :type="item.type"
-              :disabled="item.disabled"
-              :placeholder="getPlaceholder(item)"
-              @keyup.enter.native="handleFilter"
-              @blur="handleEvent($event, item.value)"
+            v-if="item.type === 'input'"
+            v-model.trim="defaultData[item.value]"
+            :size="size"
+            :style="{width: width.itemWidth + 'px'}"
+            :type="item.type"
+            :disabled="item.disabled"
+            :placeholder="getPlaceholder(item)"
+            @keyup.enter.native="handleFilter"
+            @blur="handleEvent($event, item.value)"
           />
           <!-- 日期/时间 -->
           <el-date-picker
-              v-if="item.type === 'date'"
-              v-model="defaultData[item.value]"
-              :size="size"
-              :style="{width: width.itemWidth + 'px'}"
-              :type="item.dateType"
-              :picker-options="item.TimePickerOptions"
-              :clearable="item.clearable"
-              :disabled="item.disabled"
-              start-placeholder="开始时间"
-              end-placeholder="结束时间"
-              :placeholder="getPlaceholder(item)"
-              @change="handleEvent($event, item.value, 'change')"
+            v-if="item.type === 'date'"
+            v-model="defaultData[item.value]"
+            :size="size"
+            :style="{width: width.itemWidth + 'px'}"
+            :type="item.dateType"
+            :picker-options="item.TimePickerOptions"
+            :clearable="item.clearable"
+            :disabled="item.disabled"
+            start-placeholder="开始时间"
+            end-placeholder="结束时间"
+            :placeholder="getPlaceholder(item)"
+            @change="handleEvent($event, item.value, 'change')"
           />
           <!-- 选择框 -->
           <el-select
-              v-if="item.type === 'select'"
-              v-model="defaultData[item.value]"
-              :size="size"
-              :style="{width: width.itemWidth + 'px'}"
-              :disabled="item.disabled"
-              :clearable="item.clearable"
-              :filterable="item.filterable"
-              :multiple="item.multiple"
-              :placeholder="getPlaceholder(item)"
-              @change="handleEvent($event, item.value, 'change')"
+            v-if="item.type === 'select'"
+            v-model="defaultData[item.value]"
+            :size="size"
+            :style="{width: width.itemWidth + 'px'}"
+            :disabled="item.disabled"
+            :clearable="item.clearable"
+            :filterable="item.filterable"
+            :multiple="item.multiple"
+            :placeholder="getPlaceholder(item)"
+            @change="handleEvent($event, item.value, 'change')"
           >
             <el-option
-                v-for="childItem in listTypeInfo[item.list]"
-                :key="childItem.id"
-                :label="childItem.name"
-                :value="childItem.id"
-                :disabled="childItem.disabled"
+              v-for="childItem in listTypeInfo[item.list]"
+              :key="childItem.id"
+              :label="childItem.name"
+              :value="childItem.id"
+              :disabled="childItem.disabled"
             />
           </el-select>
           <!-- 计数器 -->
           <el-input-number
-              v-if="item.type === 'inputNumber'"
-              v-model="defaultData[item.value]"
-              :size="size"
-              :style="{width: width.itemWidth + 'px'}"
-              :min="item.min"
-              :max="item.max"
-              @change="handleEvent($event, item.value, 'change')"
+            v-if="item.type === 'inputNumber'"
+            v-model="defaultData[item.value]"
+            :size="size"
+            :style="{width: width.itemWidth + 'px'}"
+            :min="item.min"
+            :max="item.max"
+            @change="handleEvent($event, item.value, 'change')"
           />
         </div>
       </div>
     </template>
     <div
-        v-if="!btnHidden && fieldList.length !== 1"
-        class="btn-style"
+      v-if="!btnHidden && fieldList.length !== 1"
+      class="btn-style"
     >
       <el-button
-          :type="btnStyle[0].type"
-          :size="size"
-          :plain="btnStyle[0].plain"
-          :round="btnStyle[0].round"
-          :icon="btnStyle[0].icon"
-          :disabled="btnStyle[0].disabled"
-          @click="handleFilter"
+        :type="btnStyle[0].type"
+        :size="size"
+        :plain="btnStyle[0].plain"
+        :round="btnStyle[0].round"
+        :icon="btnStyle[0].icon"
+        :disabled="btnStyle[0].disabled"
+        @click="handleFilter"
       >
         {{ btnStyle[0].text }}
       </el-button>
       <el-button
-          :type="btnStyle[1].type"
-          :size="size"
-          :plain="btnStyle[1].plain"
-          :round="btnStyle[1].round"
-          :disabled="btnStyle[1].disabled"
-          :icon="btnStyle[1].icon"
-          @click="handleReset"
+        :type="btnStyle[1].type"
+        :size="size"
+        :plain="btnStyle[1].plain"
+        :round="btnStyle[1].round"
+        :disabled="btnStyle[1].disabled"
+        :icon="btnStyle[1].icon"
+        @click="handleReset"
       >
         {{ btnStyle[1].text }}
       </el-button>
     </div>
     <el-button
-        v-if="!foldBtnHidden && !btnHidden && fieldList.length > count"
-        type="text"
-        @click="icon === 'open' ? icon = 'close' : icon = 'open'"
-        style="margin-left: 6px;"
+      v-if="!foldBtnHidden && !btnHidden && fieldList.length > count"
+      type="text"
+      @click="icon === 'open' ? icon = 'close' : icon = 'open'"
+      style="margin-left: 6px;"
     >{{ icon === 'open' ? '收起' : '展开' }}
       <i :class="icon === 'open' ? 'el-icon-caret-top' : 'el-icon-caret-bottom'" />
     </el-button>
